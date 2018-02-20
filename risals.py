02be28fe71060860ed7b9a95611bb5ce7aa40cce
Kazuhiro HISHINUMA <kaz@cs.meiji.ac.jp>
2018-02-20 18:07:25 +0900

    Add --purge option which regenerates all objects and caches. close #8

--- risals.py
+++ risals.py
@@ -39,7 +39,7 @@ if '__file__' in globals() and os.path.isfile(__file__):
 ###
 ### 
 ###
-def lock_s(filename: str, rich: bool=False) -> dict:
+def lock_s(filename: str, rich: bool=False, purge: bool=False) -> dict:
     img = Image.open(filename, 'r')
     if rich == True:
         thumb1 = __make_thumbnail(
@@ -47,13 +47,13 @@ def lock_s(filename: str, rich: bool=False) -> dict:
             THUMBNAIL1_WIDTH  * THUMBNAIL1_RATIO // 100,
             THUMBNAIL1_HEIGHT * THUMBNAIL1_RATIO // 100)[0]
         thumb1_rich, thumb1_w, thumb1_h = __make_thumbnail(
-            img, THUMBNAIL1_SUFFIX + '_rich', THUMBNAIL1_WIDTH, THUMBNAIL1_HEIGHT)
+            img, THUMBNAIL1_SUFFIX + '_rich', THUMBNAIL1_WIDTH, THUMBNAIL1_HEIGHT, purge=purge)
     else:
         thumb1, thumb1_w, thumb1_h = __make_thumbnail(
-            img, THUMBNAIL1_SUFFIX, THUMBNAIL1_WIDTH, THUMBNAIL1_HEIGHT)
+            img, THUMBNAIL1_SUFFIX, THUMBNAIL1_WIDTH, THUMBNAIL1_HEIGHT, purge=purge)
         thumb1_rich = thumb1
     thumb2, thumb2_w, thumb2_h = __make_thumbnail(
-        img, THUMBNAIL2_SUFFIX, THUMBNAIL2_WIDTH, THUMBNAIL2_HEIGHT)
+        img, THUMBNAIL2_SUFFIX, THUMBNAIL2_WIDTH, THUMBNAIL2_HEIGHT, purge=purge)
     return {
         "filename": filename,
         "thumbnail1": thumb1,
@@ -65,7 +65,7 @@ def lock_s(filename: str, rich: bool=False) -> dict:
         "thumbnail2_height": thumb2_h,
         }
 
-def __make_thumbnail(img: Image, suffix: str, width: int, height: int) -> Tuple[str, int, int]:
+def __make_thumbnail(img: Image, suffix: str, width: int, height: int, purge: bool=False) -> Tuple[str, int, int]:
     assert img.filename != ""
     if img.size[0] <= width and img.size[0] <= height:
         return img.filename, img.size[0], img.size[1]
@@ -73,7 +73,7 @@ def __make_thumbnail(img: Image, suffix: str, width: int, height: int) -> Tuple[
         os.makedirs(THUMBNAIL_DIR)
     prefix = os.path.splitext(img.filename)[0]
     target = os.path.join(THUMBNAIL_DIR, os.path.basename(prefix + suffix + ".jpg"))
-    if os.path.isfile(target) and max(BASETIME, os.stat(img.filename).st_mtime) < os.stat(target).st_mtime:
+    if not purge and os.path.isfile(target) and max(BASETIME, os.stat(img.filename).st_mtime) < os.stat(target).st_mtime:
         thumb = Image.open(target, 'r')
         return target, thumb.size[0], thumb.size[1]
     thumb_width  = img.size[0] * height // img.size[1]
@@ -94,6 +94,8 @@ if __name__ == '__main__':
     argparse = ArgumentParser(description='A web photo album generator')
     argparse.add_argument('-r', '--rich', action='store_true',
                           help='generate supplementary files for improving user experiences')
+    argparse.add_argument('-p', '--purge', action='store_true',
+                          help='regenerates all objects and caches')
     argparse.add_argument('-d', '--debug', action='store_true',
                           help='enable debug mode')
     argparse.add_argument('title', action='store', nargs='?', default=DEFAULT_TITLE,
@@ -103,8 +105,7 @@ if __name__ == '__main__':
     images = []
     for pattern in ['./*.jpg', './*.JPG', './*.jpeg', './*.JPEG']:
         images.extend(glob(pattern))
-    images = list(map(lock_s, tqdm(sorted(images)), itertools.repeat(args.rich)))
-    
+    images = list(map(lambda fn: lock_s(fn, rich=args.rich, purge=args.purge), tqdm(sorted(images))))
     env = Environment(
         loader=FileSystemLoader('.', 'utf-8')
         )
